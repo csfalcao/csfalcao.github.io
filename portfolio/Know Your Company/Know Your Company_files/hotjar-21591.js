5564821f338b514d1f4bf79f7e1bf0da1fb0f7d7
csfalcao <csfalcao@Mac-mini-de-Csfalcao.local>
2016-07-22 23:59:34 -0300

    Added Know Your Company to translation portfolio (ptBR)

---
+++ portfolio/Know Your Company/Know Your Company_files/hotjar-21591.js
@@ -0,0 +1,10 @@
+window.hjSiteSettings = window.hjSiteSettings || {"testers_widgets":[],"surveys":[],"heatmaps":[{"targeting":[{"negate":false,"pattern":"https:\/\/knowyourcompany.com\/story","match_operation":"simple","component":"url"}],"created_epoch_time":1425999050,"id":45011},{"targeting":[{"negate":false,"pattern":"https:\/\/knowyourcompany.com\/testimonials","match_operation":"simple","component":"url"}],"created_epoch_time":1425999023,"id":45009},{"targeting":[{"negate":false,"pattern":"https:\/\/knowyourcompany.com\/demo","match_operation":"simple","component":"url"}],"created_epoch_time":1425999005,"id":45008}],"recording_capture_keystrokes":true,"polls":[],"site_id":21591,"record_targeting_rules":[],"forms":[],"record":false,"r":1.0,"deferred_page_contents":[]};
+
+window.hjBootstrap = window.hjBootstrap || function (scriptUrl) {
+    var s = document.createElement('script');
+    s.src = scriptUrl;
+    document.getElementsByTagName('head')[0].appendChild(s);
+    window.hjBootstrap = function() {};
+};
+
+hjBootstrap('https://script.hotjar.com/modules-8686e495e0f874bea7886d14e8209c00.js');
